746712e60d81de8c6de0a857e40f9ed995c64ede
James Chen <jamesyc23@berkeley.edu>
2024-08-13 22:19:08 -0700

    finished 2.2 except for target network in DQN

--- chapter2_rl/exercises/part2_q_learning_and_dqn/answers.py
+++ chapter2_rl/exercises/part2_q_learning_and_dqn/answers.py
@@ -318,3 +318,326 @@ line(
     width=700, height=400,
 )
 # %%
+class QNetwork(nn.Module):
+    '''For consistency with your tests, please wrap your modules in a `nn.Sequential` called `layers`.'''
+    layers: nn.Sequential
+
+    def __init__(
+        self,
+        dim_observation: int,
+        num_actions: int,
+        hidden_sizes: List[int] = [120, 84]
+    ):
+        super().__init__()
+        self.dim_observation = dim_observation
+        self.num_actions = num_actions
+        self.hidden_sizes = hidden_sizes
+        self.layers = nn.Sequential(
+            nn.Linear(dim_observation, hidden_sizes[0]),
+            nn.ReLU(),
+            nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+            nn.ReLU(),
+            nn.Linear(hidden_sizes[1], num_actions)
+        )
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        return self.layers(x)
+
+
+net = QNetwork(dim_observation=4, num_actions=2)
+n_params = sum((p.nelement() for p in net.parameters()))
+assert isinstance(getattr(net, "layers", None), nn.Sequential)
+print(net)
+print(f"Total number of parameters: {n_params}")
+print("You should manually verify network is Linear-ReLU-Linear-ReLU-Linear")
+assert n_params == 10934
+# %%
+@dataclass
+class ReplayBufferSamples:
+    '''
+    Samples from the replay buffer, converted to PyTorch for use in neural network training.
+
+    Data is equivalent to (s_t, a_t, r_{t+1}, d_{t+1}, s_{t+1}).
+    '''
+    observations: Tensor # shape [sample_size, *observation_shape]
+    actions: Tensor # shape [sample_size, *action_shape]
+    rewards: Tensor # shape [sample_size,]
+    dones: Tensor # shape [sample_size,]
+    next_observations: Tensor # shape [sample_size, observation_shape]
+
+    def __post_init__(self):
+        for exp in self.__dict__.values():
+            assert isinstance(exp, Tensor), f"Error: expected type tensor, found {type(exp)}"
+
+
+class ReplayBuffer:
+    '''
+    Contains buffer; has a method to sample from it to return a ReplayBufferSamples object.
+    '''
+    rng: Generator
+    observations: np.ndarray # shape [buffer_size, *observation_shape]
+    actions: np.ndarray # shape [buffer_size, *action_shape]
+    rewards: np.ndarray # shape [buffer_size,]
+    dones: np.ndarray # shape [buffer_size,]
+    next_observations: np.ndarray # shape [buffer_size, *observation_shape]
+
+    def __init__(self, num_environments: int, obs_shape: Tuple[int], action_shape: Tuple[int], buffer_size: int, seed: int):
+        assert num_environments == 1, "This buffer only supports SyncVectorEnv with 1 environment inside."
+        self.num_environments = num_environments
+        self.obs_shape = obs_shape
+        self.action_shape = action_shape
+        self.buffer_size = buffer_size
+        self.rng = np.random.default_rng(seed)
+
+        self.observations = np.empty((0, *self.obs_shape), dtype=np.float32)
+        self.actions = np.empty(0, dtype=np.int32)
+        self.rewards = np.empty(0, dtype=np.float32)
+        self.dones = np.empty(0, dtype=bool)
+        self.next_observations = np.empty((0, *self.obs_shape), dtype=np.float32)
+
+
+    def add(
+        self, obs: np.ndarray, actions: np.ndarray, rewards: np.ndarray, dones: np.ndarray, next_obs: np.ndarray
+    ) -> None:
+        '''
+        obs: shape (num_environments, *observation_shape)
+            Observation before the action
+        actions: shape (num_environments, *action_shape)
+            Action chosen by the agent
+        rewards: shape (num_environments,)
+            Reward after the action
+        dones: shape (num_environments,)
+            If True, the episode ended and was reset automatically
+        next_obs: shape (num_environments, *observation_shape)
+            Observation after the action
+            If done is True, this should be the terminal observation, NOT the first observation of the next episode.
+        '''
+        assert obs.shape == (self.num_environments, *self.obs_shape)
+        assert actions.shape == (self.num_environments, *self.action_shape)
+        assert rewards.shape == (self.num_environments,)
+        assert dones.shape == (self.num_environments,)
+        assert next_obs.shape == (self.num_environments, *self.obs_shape) 
+
+        self.observations = np.concatenate((self.observations, obs))[-self.buffer_size:]
+        self.actions = np.concatenate((self.actions, actions))[-self.buffer_size:]
+        self.rewards = np.concatenate((self.rewards, rewards))[-self.buffer_size:]
+        self.dones = np.concatenate((self.dones, dones))[-self.buffer_size:]
+        self.next_observations = np.concatenate((self.next_observations, next_obs))[-self.buffer_size:]
+
+
+    def sample(self, sample_size: int, device: t.device) -> ReplayBufferSamples:
+        '''
+        Uniformly sample sample_size entries from the buffer and convert them to PyTorch tensors on device.
+        Sampling is with replacement, and sample_size may be larger than the buffer size.
+        '''
+        buffer_length = self.observations.shape[0]
+        indices = self.rng.integers(0, buffer_length, sample_size)
+        return ReplayBufferSamples(
+            observations=t.as_tensor(self.observations[indices]).to(device),
+            actions=t.as_tensor(self.actions[indices]).to(device),
+            rewards=t.as_tensor(self.rewards[indices]).to(device),
+            dones=t.as_tensor(self.dones[indices]).to(device),
+            next_observations=t.as_tensor(self.next_observations[indices]).to(device),
+        )
+
+
+
+tests.test_replay_buffer_single(ReplayBuffer)
+tests.test_replay_buffer_deterministic(ReplayBuffer)
+tests.test_replay_buffer_wraparound(ReplayBuffer)
+# %%
+rb = ReplayBuffer(num_environments=1, obs_shape=(4,), action_shape=(), buffer_size=256, seed=0)
+envs = gym.vector.SyncVectorEnv([make_env("CartPole-v1", 0, 0, False, "test")])
+obs = envs.reset()
+for i in range(256):
+    # Choose a random next action, and take a step in the environment
+    actions = envs.action_space.sample()
+    (next_obs, rewards, dones, infos) = envs.step(actions)
+    # Add observations to buffer, and set obs = next_obs ready for the next step
+    rb.add(obs, actions, rewards, dones, next_obs)
+    obs = next_obs
+
+plot_cartpole_obs_and_dones(rb.observations, rb.dones, title="CartPole experiences s<sub>t</sub> (dotted lines = termination)")
+
+sample = rb.sample(256, t.device("cpu"))
+plot_cartpole_obs_and_dones(sample.observations, sample.dones, title="CartPole experiences s<sub>t</sub> (randomly sampled) (dotted lines = termination)")
+# %%
+rb = ReplayBuffer(num_environments=1, obs_shape=(4,), action_shape=(), buffer_size=256, seed=0)
+envs = gym.vector.SyncVectorEnv([make_env("CartPole-v1", 0, 0, False, "test")])
+obs = envs.reset()
+for i in range(256):
+    # Choose a random next action, and take a step in the environment
+    actions = envs.action_space.sample()
+    (next_obs, rewards, dones, infos) = envs.step(actions)
+
+    # Get actual next_obs, by replacing next_obs with terminal observation at all envs which are terminated
+    real_next_obs = next_obs.copy()
+    for environment, done in enumerate(dones):
+        if done:
+            print(f'Environment {environment} terminated after {infos[0]["episode"]["l"]} steps')
+            real_next_obs[environment] = infos[environment]["terminal_observation"]
+
+    # Add the next_obs to the buffer (which has the terminated states), but set obs=new_obs (which has the restarted states)
+    rb.add(obs, actions, rewards, dones, real_next_obs)
+    obs = next_obs
+
+plot_cartpole_obs_and_dones(rb.next_observations, rb.dones, title="CartPole experiences s<sub>t+1</sub> (dotted lines = termination)")
+# %%
+def linear_schedule(
+    current_step: int, start_e: float, end_e: float, exploration_fraction: float, total_timesteps: int
+) -> float:
+    '''Return the appropriate epsilon for the current step.
+
+    Epsilon should be start_e at step 0 and decrease linearly to end_e at step (exploration_fraction * total_timesteps).
+    In other words, we are in "explore mode" with start_e >= epsilon >= end_e for the first `exploration_fraction` fraction
+    of total timesteps, and then stay at end_e for the rest of the episode.
+    '''
+    if current_step >= exploration_fraction * total_timesteps:
+        return end_e
+    else:
+        return start_e - (start_e - end_e) * (current_step / (exploration_fraction * total_timesteps))
+
+
+epsilons = [
+    linear_schedule(step, start_e=1.0, end_e=0.05, exploration_fraction=0.5, total_timesteps=500)
+    for step in range(500)
+]
+line(epsilons, labels={"x": "steps", "y": "epsilon"}, title="Probability of random action", height=400, width=600)
+
+tests.test_linear_schedule(linear_schedule)
+# %%
+def epsilon_greedy_policy(
+    envs: gym.vector.SyncVectorEnv, q_network: QNetwork, rng: Generator, obs: np.ndarray, epsilon: float
+) -> np.ndarray:
+    '''With probability epsilon, take a random action. Otherwise, take a greedy action according to the q_network.
+    Inputs:
+        envs : gym.vector.SyncVectorEnv, the family of environments to run against
+        q_network : QNetwork, the network used to approximate the Q-value function
+        obs : The current observation
+        epsilon : exploration percentage
+    Outputs:
+        actions: (n_environments, *action_shape) the sampled action for each environment.
+    '''
+    # Convert `obs` into a tensor so we can feed it into our model
+    device = next(q_network.parameters()).device
+    obs = t.from_numpy(obs).to(device)
+
+    if rng.random() < epsilon:
+        return rng.integers(0, envs.single_action_space.n, (envs.num_envs,))
+    else:
+        q_values = q_network(obs).detach().numpy()
+        return np.argmax(q_values, axis=1)
+
+
+tests.test_epsilon_greedy_policy(epsilon_greedy_policy)
+# %%
+@dataclass
+class DQNArgs:
+    # Basic / global
+    seed: int = 1
+    cuda: bool = t.cuda.is_available()
+    env_id: str = "CartPole-v1"
+
+    # Wandb / logging
+    use_wandb: bool = False
+    capture_video: bool = True
+    exp_name: str = "DQN_implementation"
+    log_dir: str = "logs"
+    wandb_project_name: str = "CartPoleDQN"
+    wandb_entity: Optional[str] = None
+
+    # Duration of different phases
+    buffer_size: int = 10_000
+    train_frequency: int = 10
+    total_timesteps: int = 500_000
+    target_network_frequency: int = 500
+
+    # Optimization hyperparameters
+    batch_size: int = 128
+    learning_rate: float = 0.00025
+    start_e: float = 1.0
+    end_e: float = 0.1
+
+    # Misc. RL related
+    gamma: float = 0.99
+    exploration_fraction: float = 0.2
+
+    def __post_init__(self):
+        assert self.total_timesteps - self.buffer_size >= self.train_frequency
+        self.total_training_steps = (self.total_timesteps - self.buffer_size) // self.train_frequency
+
+
+args = DQNArgs(batch_size=256)
+utils.arg_help(args)
+# %%
+class DQNAgent:
+    '''Base Agent class handling the interaction with the environment.'''
+
+    def __init__(
+        self,
+        envs: gym.vector.SyncVectorEnv,
+        args: DQNArgs,
+        rb: ReplayBuffer,
+        q_network: QNetwork,
+        target_network: QNetwork,
+        rng: np.random.Generator
+    ):
+        self.envs = envs
+        self.args = args
+        self.rb = rb
+        self.next_obs = self.envs.reset() # Need a starting observation!
+        self.step = 0
+        self.epsilon = args.start_e
+        self.q_network = q_network
+        self.target_network = target_network
+        self.rng = rng
+
+    def play_step(self) -> List[dict]:
+        '''
+        Carries out a single interaction step between the agent and the environment, and adds results to the replay buffer.
+
+        Returns `infos` (list of dictionaries containing info we will log).
+        '''
+        obs = self.next_obs
+        actions = self.get_actions(obs)
+        (next_obs, rewards, dones, infos) = self.envs.step(actions)
+
+        real_next_obs = next_obs.copy()
+        for environment, done in enumerate(dones):
+            if done:
+                print(f'Environment {environment} terminated after {infos[0]["episode"]["l"]} steps')
+                real_next_obs[environment] = infos[environment]["terminal_observation"]
+
+        self.rb.add(obs, actions, rewards, dones, real_next_obs)
+        self.next_obs = real_next_obs
+        self.step += 1
+
+        return infos
+
+    # Add the next_obs to the buffer (which has the terminated states), but set obs=new_obs (which has the restarted states)
+    rb.add(obs, actions, rewards, dones, real_next_obs)
+
+    def get_actions(self, obs: np.ndarray) -> np.ndarray:
+        '''
+        Samples actions according to the epsilon-greedy policy using the linear schedule for epsilon.
+        '''
+        self.epsilon = linear_schedule(
+            current_step=self.step,
+            start_e=self.args.start_e,
+            end_e=self.args.end_e,
+            exploration_fraction=self.args.exploration_fraction,
+            total_timesteps=self.args.total_timesteps,
+        )
+        
+        actions = epsilon_greedy_policy(
+            envs=self.envs,
+            q_network=self.q_network,
+            rng=self.rng,
+            obs=obs,
+            epsilon=self.epsilon,
+        )
+
+        return actions
+
+
+tests.test_agent(DQNAgent)
