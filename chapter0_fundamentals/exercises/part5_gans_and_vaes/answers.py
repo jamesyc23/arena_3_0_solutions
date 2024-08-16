3fd6f807a1f83530d98c57b6e1fe89bb7846804c
James Chen <jamesyc23@berkeley.edu>
2024-08-15 17:45:04 -0700

    finished 0.5

--- chapter0_fundamentals/exercises/part5_gans_and_vaes/answers.py
+++ chapter0_fundamentals/exercises/part5_gans_and_vaes/answers.py
@@ -110,12 +110,12 @@ def initialize_weights(model: nn.Module) -> None:
     '''
     Initializes weights according to the DCGAN paper, by modifying model weights in place.
     '''
-    for layer in model.modules():
-        if isinstance(layer, BatchNorm2d):
-            nn.init.normal_(layer.weight, mean=1, std=0.02)
-            nn.init.constant_(layer.bias, val=0)
-        else:
-            nn.init.normal_(layer.weight, mean=0, std=0.02)
+    for (name, module) in model.named_modules():
+        if isinstance(module, BatchNorm2d):
+            nn.init.normal_(module.weight, mean=1, std=0.02)
+            nn.init.constant_(module.bias, val=0)
+        elif any([isinstance(module, Module) for Module in [Conv2d, ConvTranspose2d, Linear]]):
+            nn.init.normal_(module.weight, mean=0, std=0.02)
 
 
 tests.test_initialize_weights(initialize_weights, ConvTranspose2d, Conv2d, Linear, BatchNorm2d)
@@ -228,8 +228,6 @@ class Discriminator(nn.Module):
             Sigmoid(),
         )
 
-        initialize_weights(self.model)
-
     def forward(self, x: t.Tensor) -> t.Tensor:
         return self.model(x)
 
@@ -261,6 +259,8 @@ class DCGAN(nn.Module):
             img_channels,
             hidden_channels,
         )
+
+        initialize_weights(self)
 
 print_param_count(Generator(), solutions.DCGAN().netG)
 print_param_count(Discriminator(), solutions.DCGAN().netD)
@@ -475,4 +475,350 @@ args = DCGANArgs(
 )
 trainer = DCGANTrainer(args)
 trainer.train()
+# %%
+class Autoencoder(nn.Module):
+
+    def __init__(self, latent_dim_size: int, hidden_dim_size: int):
+        super().__init__()
+        self.latent_dim_size = latent_dim_size
+        self.hidden_dim_size = hidden_dim_size
+
+        self.encoder = Sequential(
+            Conv2d(in_channels=1, out_channels=16, kernel_size=4, stride=2, padding=1),
+            ReLU(),
+            Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2, padding=1),
+            ReLU(),
+            nn.Flatten(),
+            Linear(in_features=32*7*7, out_features=hidden_dim_size),
+            ReLU(),
+            Linear(in_features=hidden_dim_size, out_features=latent_dim_size),
+        )
+
+        self.decoder = Sequential(
+            Linear(in_features=latent_dim_size, out_features=hidden_dim_size),
+            ReLU(),
+            Linear(in_features=hidden_dim_size, out_features=32*7*7),
+            nn.Unflatten(1, (32, 7, 7)),
+            ReLU(),
+            ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=4, stride=2, padding=1),
+            ReLU(),
+            ConvTranspose2d(in_channels=16, out_channels=1, kernel_size=4, stride=2, padding=1),
+        )
+
+    def forward(self, x: t.Tensor) -> t.Tensor:
+        z = self.encoder(x)
+        x_prime = self.decoder(z)
+        return x_prime
+    
+soln_Autoencoder = solutions.Autoencoder(latent_dim_size=5, hidden_dim_size=128)
+my_Autoencoder = Autoencoder(latent_dim_size=5, hidden_dim_size=128)
+
+print_param_count(my_Autoencoder, soln_Autoencoder)
+# %%
+testset = get_dataset("MNIST", train=False)
+HOLDOUT_DATA = dict()
+for data, target in DataLoader(testset, batch_size=1):
+    if target.item() not in HOLDOUT_DATA:
+        HOLDOUT_DATA[target.item()] = data.squeeze()
+        if len(HOLDOUT_DATA) == 10: break
+HOLDOUT_DATA = t.stack([HOLDOUT_DATA[i] for i in range(10)]).to(dtype=t.float, device=device).unsqueeze(1)
+
+display_data(HOLDOUT_DATA, nrows=2, title="MNIST holdout data")
+# %%
+@dataclass
+class AutoencoderArgs():
+    latent_dim_size: int = 5
+    hidden_dim_size: int = 128
+    dataset: Literal["MNIST", "CELEB"] = "MNIST"
+    batch_size: int = 512
+    epochs: int = 10
+    lr: float = 1e-3
+    betas: Tuple[float, float] = (0.5, 0.999)
+    seconds_between_eval: int = 5
+    wandb_project: Optional[str] = 'day5-ae-mnist'
+    wandb_name: Optional[str] = None
+
+
+class AutoencoderTrainer:
+    def __init__(self, args: AutoencoderArgs):
+        self.args = args
+        self.trainset = get_dataset(args.dataset)
+        self.trainloader = DataLoader(self.trainset, batch_size=args.batch_size, shuffle=True)
+        self.model = Autoencoder(
+            latent_dim_size = args.latent_dim_size,
+            hidden_dim_size = args.hidden_dim_size,
+        ).to(device)
+        self.optimizer = t.optim.Adam(self.model.parameters(), lr=args.lr, betas=args.betas)
+
+    def training_step(self, img: t.Tensor) -> t.Tensor:
+        '''
+        Performs a training step on the batch of images in `img`. Returns the loss.
+        '''
+        self.model.zero_grad()
+        img_prime = self.model(img)
+        loss = ((img - img_prime) ** 2).mean()
+        loss.backward()
+        self.optimizer.step()
+        return loss
+
+    @t.inference_mode()
+    def evaluate(self) -> None:
+        '''
+        Evaluates model on holdout data, logs to weights & biases.
+        '''
+        holdout_prime = self.model(HOLDOUT_DATA)
+        holdout_prime_np = holdout_prime.permute(0, 2, 3, 1).numpy()
+        imgs = [wandb.Image(img) for img in holdout_prime_np]
+        wandb.log({"images": imgs}, step=self.step)
+
+    def train(self) -> None:
+        '''
+        Performs a full training run, logging to wandb.
+        '''
+        self.step = 0
+        last_log_time = time.time()
+        wandb.init(project=self.args.wandb_project, name=self.args.wandb_name)
+        wandb.watch(self.model)
+
+        for epoch in range(self.args.epochs):
+
+            progress_bar = tqdm(self.trainloader, total=int(len(self.trainloader)))
+
+            for i, (img, label) in enumerate(progress_bar): # remember that label is not used
+
+                img = img.to(device)
+                loss = self.training_step(img)
+                wandb.log(dict(loss=loss), step=self.step)
+
+                # Update progress bar
+                self.step += img.shape[0]
+                progress_bar.set_description(f"{epoch=}, {loss=:.4f}, examples_seen={self.step}")
+
+                # Evaluate model on the same holdout data
+                if time.time() - last_log_time > self.args.seconds_between_eval:
+                    last_log_time = time.time()
+                    self.evaluate()
+
+        wandb.finish()
+
+
+args = AutoencoderArgs()
+trainer = AutoencoderTrainer(args)
+trainer.train()
+# %%
+@t.inference_mode()
+def visualise_output(
+    model: Autoencoder,
+    n_points: int = 11,
+    interpolation_range: Tuple[float, float] = (-3, 3),
+) -> None:
+    '''
+    Visualizes the output of the decoder, along the first two latent dims.
+    '''
+    # Constructing latent dim data by making two of the dimensions vary indep in the interpolation range
+    grid_latent = t.zeros(n_points**2, model.latent_dim_size).to(device)
+    x = t.linspace(*interpolation_range, n_points).to(device)
+    grid_latent[:, 0] = einops.repeat(x, "dim1 -> (dim1 dim2)", dim2=n_points)
+    grid_latent[:, 1] = einops.repeat(x, "dim2 -> (dim1 dim2)", dim1=n_points)
+
+    # Pass through decoder
+    output = model.decoder(grid_latent).cpu().numpy()
+
+    # Normalize & truncate, then unflatten back into a grid shape
+    output_truncated = np.clip((output * 0.3081) + 0.1307, 0, 1)
+    output_single_image = einops.rearrange(
+        output_truncated,
+        "(dim1 dim2) 1 height width -> (dim1 height) (dim2 width)",
+        dim1=n_points
+    )
+
+    # Display the results
+    px.imshow(
+        output_single_image,
+        color_continuous_scale="greys_r",
+        title="Decoder output from varying first principal components of latent space"
+    ).update_layout(
+        xaxis=dict(title_text="dim1", tickmode="array", tickvals=list(range(14, 14+28*n_points, 28)), ticktext=[f"{i:.2f}" for i in x]),
+        yaxis=dict(title_text="dim2", tickmode="array", tickvals=list(range(14, 14+28*n_points, 28)), ticktext=[f"{i:.2f}" for i in x])
+    ).show()
+
+
+visualise_output(trainer.model)
+#%%
+@t.inference_mode()
+def visualise_input(
+    model: Autoencoder,
+    dataset: Dataset,
+) -> None:
+    '''
+    Visualises (in the form of a scatter plot) the input data in the latent space, along the first two dims.
+    '''
+    # First get the model images' latent vectors, along first 2 dims
+    imgs = t.stack([batch for batch, label in dataset]).to(device)
+    latent_vectors = model.encoder(imgs)
+    if latent_vectors.ndim == 3: latent_vectors = latent_vectors[0] # useful for VAEs later
+    latent_vectors = latent_vectors[:, :2].cpu().numpy()
+    labels = [str(label) for img, label in dataset]
+
+    # Make a dataframe for scatter (px.scatter is more convenient to use when supplied with a dataframe)
+    df = pd.DataFrame({"dim1": latent_vectors[:, 0], "dim2": latent_vectors[:, 1], "label": labels})
+    df = df.sort_values(by="label")
+    fig = px.scatter(df, x="dim1", y="dim2", color="label")
+    fig.update_layout(height=700, width=700, title="Scatter plot of latent space dims", legend_title="Digit")
+    data_range = df["dim1"].max() - df["dim1"].min()
+
+    # Add images to the scatter plot (optional)
+    output_on_data_to_plot = model.encoder(HOLDOUT_DATA.to(device))[:, :2].cpu()
+    if output_on_data_to_plot.ndim == 3: output_on_data_to_plot = output_on_data_to_plot[0] # useful for VAEs; see later
+    data_translated = (HOLDOUT_DATA.cpu().numpy() * 0.3081) + 0.1307
+    data_translated = (255 * data_translated).astype(np.uint8).squeeze()
+    for i in range(10):
+        x, y = output_on_data_to_plot[i]
+        fig.add_layout_image(
+            source=Image.fromarray(data_translated[i]).convert("L"),
+            xref="x", yref="y",
+            x=x, y=y,
+            xanchor="right", yanchor="top",
+            sizex=data_range/15, sizey=data_range/15,
+        )
+    fig.show()
+
+
+small_dataset = Subset(get_dataset("MNIST"), indices=range(0, 5000))    
+visualise_input(trainer.model, small_dataset)
+
+#%%
+class VAE(nn.Module):
+    encoder: nn.Module
+    decoder: nn.Module
+
+    def __init__(self, latent_dim_size: int, hidden_dim_size: int):
+        super().__init__()
+        self.latent_dim_size = latent_dim_size
+        self.hidden_dim_size = hidden_dim_size
+
+        self.encoder = Sequential(
+            Conv2d(in_channels=1, out_channels=16, kernel_size=4, stride=2, padding=1),
+            ReLU(),
+            Conv2d(in_channels=16, out_channels=32, kernel_size=4, stride=2, padding=1),
+            ReLU(),
+            nn.Flatten(),
+            Linear(in_features=32*7*7, out_features=hidden_dim_size),
+            ReLU(),
+            Linear(in_features=hidden_dim_size, out_features=2*latent_dim_size),
+        )
+
+        self.decoder = Sequential(
+            Linear(in_features=latent_dim_size, out_features=hidden_dim_size),
+            ReLU(),
+            Linear(in_features=hidden_dim_size, out_features=32*7*7),
+            nn.Unflatten(1, (32, 7, 7)),
+            ReLU(),
+            ConvTranspose2d(in_channels=32, out_channels=16, kernel_size=4, stride=2, padding=1),
+            ReLU(),
+            ConvTranspose2d(in_channels=16, out_channels=1, kernel_size=4, stride=2, padding=1),
+        )
+
+    def sample_latent_vector(self, x: t.Tensor) -> Tuple[t.Tensor, t.Tensor, t.Tensor]:
+        '''
+        Passes `x` through the encoder. Returns the mean and log std dev of the latent vector,
+        as well as the latent vector itself. This function can be used in `forward`, but also
+        used on its own to generate samples for evaluation.
+        '''
+        encoder_out = self.encoder(x)
+        mu, logsigma = encoder_out[:, :self.latent_dim_size], encoder_out[:, self.latent_dim_size:]
+        z = mu + t.exp(logsigma) * t.randn_like(mu)
+        return (z, mu, logsigma)
+
+    def forward(self, x: t.Tensor) -> Tuple[t.Tensor, t.Tensor, t.Tensor]:
+        '''
+        Passes `x` through the encoder and decoder. Returns the reconstructed input, as well
+        as mu and logsigma.
+        '''
+        z, mu, logsigma = self.sample_latent_vector(x)
+        x_prime = self.decoder(z)
+        return (x_prime, mu, logsigma)
+
+model = VAE(latent_dim_size=5, hidden_dim_size=100)
+
+trainset_mnist = get_dataset("MNIST")
+x = next(iter(DataLoader(trainset_mnist, batch_size=8)))[0]
+print(torchinfo.summary(model, input_data=x))
+# %%
+@dataclass
+class VAEArgs(AutoencoderArgs):
+    wandb_project: Optional[str] = 'day5-vae-mnist'
+    beta_kl: float = 0.1
+
+
+class VAETrainer:
+    def __init__(self, args: VAEArgs):
+        self.args = args
+        self.trainset = get_dataset(args.dataset)
+        self.trainloader = DataLoader(self.trainset, batch_size=args.batch_size, shuffle=True)
+        self.model = VAE(
+            latent_dim_size = args.latent_dim_size,
+            hidden_dim_size = args.hidden_dim_size,
+        ).to(device)
+        self.optimizer = t.optim.Adam(self.model.parameters(), lr=args.lr, betas=args.betas)
+
+    def training_step(self, img: t.Tensor) -> t.Tensor:
+        '''
+        Performs a training step on the batch of images in `img`. Returns the loss.
+        '''
+        self.model.zero_grad()
+        img_prime, mu, logsigma = self.model(img)
+        kl_div = ((t.exp(logsigma) ** 2 + mu ** 2 - 1) / 2 - logsigma).mean()
+        loss = ((img - img_prime) ** 2).mean() + self.args.beta_kl * kl_div
+        loss.backward()
+        self.optimizer.step()
+        return loss
+
+    @t.inference_mode()
+    def evaluate(self) -> None:
+        '''
+        Evaluates model on holdout data, logs to weights & biases.
+        '''
+        holdout_prime, _, _ = self.model(HOLDOUT_DATA)
+        holdout_prime_np = holdout_prime.permute(0, 2, 3, 1).numpy()
+        imgs = [wandb.Image(img) for img in holdout_prime_np]
+        wandb.log({"images": imgs}, step=self.step)
+
+    def train(self) -> None:
+        '''
+        Performs a full training run, logging to wandb.
+        '''
+        self.step = 0
+        last_log_time = time.time()
+        wandb.init(project=self.args.wandb_project, name=self.args.wandb_name)
+        wandb.watch(self.model)
+
+        for epoch in range(self.args.epochs):
+
+            progress_bar = tqdm(self.trainloader, total=int(len(self.trainloader)))
+
+            for i, (img, label) in enumerate(progress_bar): # remember that label is not used
+
+                img = img.to(device)
+                loss = self.training_step(img)
+                wandb.log(dict(loss=loss), step=self.step)
+
+                # Update progress bar
+                self.step += img.shape[0]
+                progress_bar.set_description(f"{epoch=}, {loss=:.4f}, examples_seen={self.step}")
+
+                # Evaluate model on the same holdout data
+                if time.time() - last_log_time > self.args.seconds_between_eval:
+                    last_log_time = time.time()
+                    self.evaluate()
+
+        wandb.finish()
+
+
+args = VAEArgs(latent_dim_size=10, hidden_dim_size=100)
+trainer = VAETrainer(args)
+trainer.train()
+# %%
+visualise_output(trainer.model)
+visualise_input(trainer.model, small_dataset)
 # %%
